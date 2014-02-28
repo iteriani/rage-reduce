967fe03e00b3a865375277361e2c8fe6ca9fb5d6
iteriani <fortrex@gmail.com>
2014-02-27 21:30:27 -0800

    enrolled bug

--- controllers/classes.js
+++ controllers/classes.js
@@ -4,21 +4,26 @@ var mongoose = require('mongoose'),
 
 exports.getClasses = function(req,res){
 	var searchQuery = {};
-	console.log(req.params);
 	if(req.params.id != null){
 		searchQuery.id = req.params.id;
 	}
 	Class.find(searchQuery, function(err, classes){
-		console.log(classes.length)
 		var returner = []
 		for(var i = 0; i < classes.length; i++){
 			var _class = classes[i];
-
+			var enrolled = false;
+			if(req.session.user != null){
+				for(var m = 0; m < _class.enrollment.length; m++){
+					if(_class.enrollment[m].pid == req.session.user.pid){
+						enrolled = true;
+					}
+				}
+			}
 			var obj = {
 						id : _class.id,
 						name : _class.name,
 						enrollment : _class.enrollment.length,
-						enrolled : req.session.user != null && _class.enrollment.indexOf(req.session.user.pid) >= 0,
+						enrolled : enrolled,
 						instructor : _class.instructor,
 						max_enrollment : _class.max_enrollment,
 						group : _class.group
@@ -34,7 +39,6 @@ exports.getClasses = function(req,res){
 					})
 
 					obj.info = realdata[0];
-					console.log(obj.info);
 					res.end(JSON.stringify(obj, null, '\t'));
 					return;
 				})
