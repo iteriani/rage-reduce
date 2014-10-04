3884e5194ca46726ca993c4d13ff1aa33fc41589
Samuel Ko <samuelsko@gmail.com>
2014-10-04 00:50:44 -0700

    stuff

--- app.js
+++ app.js
@@ -51,6 +51,9 @@ server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
+app.get('/', function(req, res) {
+	res.render('index');
+});
 app.get("/suggestMessage", function(req,res){
 	console.log(req.query.message);
 	res.end(req.query.message);
--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	db : "mongodb://iteriani:102092Aa@troup.mongohq.com:10059/webregpro"
+	db : "mongodb://test:test@kahana.mongohq.com:10089/TritonSX"
 	//db: "localhost:27017"
 }
